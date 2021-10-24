7dd8b1ec17f9488f3d514a26ab70a167246ad741
ttofTnT <644809641@qq.com>
2021-10-24 16:14:21 +0800

    hash table

---
+++ leetcode/Hash Table/242. Valid Anagram.go
@@ -0,0 +1,22 @@
+package Hash_Table
+
+func isAnagram(s string, t string) bool {
+	alphabet := make([]int, 26)
+	sBytes := []byte(s)
+	tBytes := []byte(t)
+	if len(sBytes) != len(tBytes) {
+		return false
+	}
+	for i := 0; i < len(sBytes); i++ {
+		alphabet[sBytes[i]-'a']++
+	}
+	for i := 0; i < len(tBytes); i++ {
+		alphabet[tBytes[i]-'a']--
+	}
+	for i := 0; i < 26; i++ {
+		if alphabet[i] != 0 {
+			return false
+		}
+	}
+	return true
+}
