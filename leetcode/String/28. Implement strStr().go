57e499f3760c15172b5515ef3488876fe34650ae
ttofTnT <644809641@qq.com>
2021-10-30 09:14:45 +0800

    不会双指针

---
+++ leetcode/String/28. Implement strStr().go
@@ -0,0 +1,18 @@
+package String
+
+//暴力搜索
+func strStr(haystack string, needle string) int {
+	for i := 0; ; i++ {
+		for j := 0; ; j++ {
+			if j == len(needle) {
+				return i
+			}
+			if i+j == len(haystack) {
+				return -1
+			}
+			if needle[j] != haystack[i+j] {
+				break
+			}
+		}
+	}
+}
--- leetcode/String/剑指 Offer 58 - II. 左旋转字符串.go
+++ leetcode/String/剑指 Offer 58 - II. 左旋转字符串.go
@@ -4,13 +4,13 @@ func reverseLeftWords(s string, n int) string {
 	// 1. 反转前n个字符
 	// 2. 反转第n到end字符
 	// 3. 反转整个字符
-	reverse(b, 0, n-1)
-	reverse(b, n, len(b)-1)
-	reverse(b, 0, len(b)-1)
+	reverse58(b, 0, n-1)
+	reverse58(b, n, len(b)-1)
+	reverse58(b, 0, len(b)-1)
 	return string(b)
 }
 // 切片是引用传递
-func reverse(b []byte, left, right int){
+func reverse58(b []byte, left, right int){
 	for left < right{
 		b[left], b[right] = b[right],b[left]
 		left++
